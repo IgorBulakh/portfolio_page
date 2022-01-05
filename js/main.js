b064568c1235824e1da9cfe3bab3fd5f41185cf8
IgorBulakh <bulach.igor@gmail.com>
2022-01-05 17:30:11 +0300

    all page

--- js/main.js
+++ js/main.js
@@ -1,9 +1,14 @@
 $(function () {
-  $(".slider").slick({
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    adaptiveHeight: true,
-  });
+  var mixer = mixitup(".portfolio__content");
+
+  animation: {
+    duration: 700;
+  }
+  // $(".slider").slick({
+  //   dots: true,
+  //   infinite: true,
+  //   speed: 300,
+  //   slidesToShow: 1,
+  //   adaptiveHeight: true,
+  // });
 });
