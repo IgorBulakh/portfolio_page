af3b0243b2674dcd521f773c001f99eabc05a851
IgorBulakh <bulach.igor@gmail.com>
2022-02-07 15:47:41 +0300

    2 variants js scrol

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 $(function () {
+  $(".menu__link, .logo, .header__link").on("click", function (event) {
+    event.preventDefault();
+
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
+  /* 
+  $(window).scroll(function () {
+    var heightScroll = $(window).offset().top;
+    
+    if (heightScroll > 100) {
+      $(".header__inner").addClass("header-fixed");
+    } else {
+      $(".header__inner").removeClass("header-fixed");
+    }
+  });  
+*/
+
+  $(window).scroll(function () {
+    var heightScroll = $(window).scrollTop();
+
+    if (heightScroll > 128) {
+      $(".header__inner").addClass("header__fixed");
+    } else {
+      $(".header__inner").removeClass("header__fixed");
+    }
+  });
+
   animation: {
     duration: 700;
   }
@@ -10,7 +40,7 @@ $(function () {
   //   adaptiveHeight: true,
   // });
 
-  $(".burger").on("click", function () {
+  $(".burger, .menu a").on("click", function () {
     $(".burger").toggleClass("burger--active");
     $(".menu").toggleClass("menu__active");
   });
